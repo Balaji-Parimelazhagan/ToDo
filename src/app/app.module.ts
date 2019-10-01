5f69f9e0a5c35bebf1f712f21d5aa63aa99887d7
Balaji_Parimelazhagan <balaji@ideas2it.com>
2019-10-01 21:12:57 +0530

    angular-basic-layout-completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidePanelComponent } from './side-panel/side-panel.component';
+import { ListDetailComponent } from './list-detail/list-detail.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidePanelComponent,
+    ListDetailComponent,
+    TaskDetailComponent
   ],
   imports: [
     BrowserModule,
